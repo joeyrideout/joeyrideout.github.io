2c3e3ac64e06678fe839d852be5acbe37a8085c4
Joey Rideout <joeyrrideout@gmail.com>
2015-12-01 23:33:18 -0800

    Fully minified site with no unused CSS rules.

--- build.js
+++ build.js
@@ -1,30 +1,60 @@
 var hm = require("html-minifier");
 var nm = require("node-minify");
 var fs = require('fs');
-fs.readFile('index_source.html', 'utf8', function (err,data) {
-  if (err) {
-    return console.log(err);
-  }
-  var output = hm.minify(data, {
-    removeComments: true,
-    removeCommentsFromCDATA: true,
-    collapseBooleanAttributes: true,
-    collapseWhitespace: true,
-    removeEmptyAttributes: true
-  });
-  fs.writeFile('index.html', output, function (err,data) {
+var uncss = require('uncss');
+
+function minifyAndCleanHtml() {
+  fs.readFile('index_source.html', 'utf8', function (err,data) {
     if (err) {
       return console.log(err);
     }
-    console.log(output);
-  });
-});
+
+    var output = hm.minify(data, {
+      removeComments: true,
+      removeCommentsFromCDATA: true,
+      collapseBooleanAttributes: true,
+      collapseWhitespace: true,
+      removeEmptyAttributes: true
+    });
+
+    options = {
+      stylesheets  : ['css/joeyrideout.blob.css']
+    };
+    uncss(output, options, function (error, output) {
+        fs.writeFile('css/joeyrideout.clean.blob.css', output, function (err,data) {
+          if (err) {
+            return console.log(err);
+          }
+          new nm.minify({
+            type: 'yui-css',
+            fileIn: ['css/joeyrideout.clean.blob.css'],
+            fileOut: 'css/joeyrideout.min.css',
+            callback: function(err, min){
+              if (err) {
+                return console.log(err);
+              }
+            }
+          });
+        });
+    });
+
+    fs.writeFile('index.html', output, function (err,data) {
+      if (err) {
+        return console.log(err);
+      }
+    });
+  })
+}
 
 new nm.minify({
   type: 'yui-css',
   fileIn: ['css/font.css', 'css/normalize.css', 'css/skeleton.css', 'css/custom.css'],
-  fileOut: 'css/joeyrideout.min.css',
+  fileOut: 'css/joeyrideout.blob.css',
   callback: function(err, min){
-    console.log(err);
+    if (err) {
+      return console.log(err);
+    }
+    minifyAndCleanHtml();
   }
 });
+
