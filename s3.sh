d5050d84da4c8f83afdff773f74194f400e84bf1
joeyrideout <joeyrrideout@gmail.com>
2016-07-10 23:21:01 +0000

    Commenting out AWS upload command of images folder since it's rarely useful.

--- s3.sh
+++ s3.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 aws s3 cp index.html s3://joeyrideout.com --acl public-read
 aws s3 cp therideouthideout.html s3://joeyrideout.com --acl public-read
-aws s3 cp images/ s3://joeyrideout.com/images/ --recursive --acl public-read
+#aws s3 cp images/ s3://joeyrideout.com/images/ --recursive --acl public-read
